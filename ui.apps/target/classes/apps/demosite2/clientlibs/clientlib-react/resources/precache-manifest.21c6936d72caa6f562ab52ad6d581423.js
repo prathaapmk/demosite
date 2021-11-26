597364bd0fcb703e5ede6f5b4b05e3be6e519f68
Prathap Mullaguri <pramulla3@publicisgroupe.net>
2021-11-26 08:09:03 +0530

    First commit

---
+++ ui.apps/target/classes/apps/demosite2/clientlibs/clientlib-react/resources/precache-manifest.21c6936d72caa6f562ab52ad6d581423.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb4b0caf3f0956d54ff4c9716a2fe1d2",
+    "url": "/etc.clientlibs/demosite2/clientlibs/clientlib-react/resources/index.html"
+  },
+  {
+    "revision": "e842e4d17d3a40d5d6f9",
+    "url": "/etc.clientlibs/demosite2/clientlibs/clientlib-react/resources/static/css/4.9c303500.chunk.css"
+  },
+  {
+    "revision": "9ce710466e2b2076733a",
+    "url": "/etc.clientlibs/demosite2/clientlibs/clientlib-react/resources/static/css/main.01227443.chunk.css"
+  },
+  {
+    "revision": "785b9721e5eb1559f02f",
+    "url": "/etc.clientlibs/demosite2/clientlibs/clientlib-react/resources/static/js/2.fc393910.chunk.js"
+  },
+  {
+    "revision": "a112985e346e36813f8faa2e1e345c78",
+    "url": "/etc.clientlibs/demosite2/clientlibs/clientlib-react/resources/static/js/2.fc393910.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42d9fa7d46dad194b79a",
+    "url": "/etc.clientlibs/demosite2/clientlibs/clientlib-react/resources/static/js/3.fdcfd73a.chunk.js"
+  },
+  {
+    "revision": "c1a19abb78cf9cde35b8b3bd4cf9adc0",
+    "url": "/etc.clientlibs/demosite2/clientlibs/clientlib-react/resources/static/js/3.fdcfd73a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e842e4d17d3a40d5d6f9",
+    "url": "/etc.clientlibs/demosite2/clientlibs/clientlib-react/resources/static/js/4.a7fee02f.chunk.js"
+  },
+  {
+    "revision": "dea230911350f1a60cc9",
+    "url": "/etc.clientlibs/demosite2/clientlibs/clientlib-react/resources/static/js/5.e8b0e9d3.chunk.js"
+  },
+  {
+    "revision": "baa600eba6c59a901e60",
+    "url": "/etc.clientlibs/demosite2/clientlibs/clientlib-react/resources/static/js/6.a534198b.chunk.js"
+  },
+  {
+    "revision": "9ce710466e2b2076733a",
+    "url": "/etc.clientlibs/demosite2/clientlibs/clientlib-react/resources/static/js/main.7995857d.chunk.js"
+  },
+  {
+    "revision": "363c7fd78e6de215adeb",
+    "url": "/etc.clientlibs/demosite2/clientlibs/clientlib-react/resources/static/js/runtime-main.8c477a97.js"
+  }
+]);
